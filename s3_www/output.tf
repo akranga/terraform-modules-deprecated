23c91a37261cd5b5a22ccc86b32915935b99a788
Antons Kranga <anton@agilestacks.com>
2016-10-27 16:47:24 +0300

    added website endpoint output var

--- s3_www/output.tf
+++ s3_www/output.tf
@@ -6,6 +6,10 @@ output "website_domain" {
   value = "${aws_s3_bucket.main.website_domain}"
 }
 
+output "website_endpoint" {
+  value = "${aws_s3_bucket.main.website_endpoint}"
+}
+
 output "hosted_zone_id" {
   value = "${aws_s3_bucket.main.hosted_zone_id}"
 }
