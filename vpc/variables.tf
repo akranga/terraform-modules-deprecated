d6096a1b6fdb4e3faf2c068cc3242bb2f14b1b68
Antons Kranga <anton@agilestacks.com>
2016-11-01 00:01:23 +0200

    added dns opts for vpc

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -5,3 +5,15 @@ variable "assign_public_ip" {
   description = "Assign public ip for subnet"
   value = "false"
 }
+
+variable "domain_name" {
+  type = "string"
+  description = "domain name associated to vpc instances"
+  default = "internal"
+}
+
+variable "dns_servers" {
+  type = "list"
+  description = "Default DNS recursors"
+  default = ["10.0.0.2", "8.8.8.8"]
+}
