ac172e7595f26abd54dc91963cf803075e11ace5
Antons Kranga <akranga@gmail.com>
2016-11-07 23:41:57 +0200

    commented out cloudwatch for better time

--- sns/main.tf
+++ sns/main.tf
@@ -2,22 +2,22 @@ resource "aws_sns_topic" "main" {
   name = "${var.name}"
 }
 
-resource "aws_cloudwatch_log_group" "main" {
-  name = "${var.name}"
-  retention_in_days = 1
-}
+# resource "aws_cloudwatch_log_group" "main" {
+#   name = "${var.name}"
+#   retention_in_days = 1
+# }
 
-resource "aws_cloudwatch_log_stream" "main" {
-  name           = "${var.name}"
-  log_group_name = "${aws_cloudwatch_log_group.main.name}"
-}
+# resource "aws_cloudwatch_log_stream" "main" {
+#   name           = "${var.name}"
+#   log_group_name = "${aws_cloudwatch_log_group.main.name}"
+# }
 
-resource "aws_sns_topic_subscription" "subs" {
-    topic_arn = "${aws_sns_topic.main.arn}"
-    protocol  = "application"
-    endpoint  = "${aws_cloudwatch_log_group.main.name}"
-    endpoint_auto_confirms = true
-}
+# resource "aws_sns_topic_subscription" "subs" {
+#     topic_arn = "${aws_sns_topic.main.arn}"
+#     protocol  = "application"
+#     endpoint  = "${aws_cloudwatch_log_group.main.name}"
+#     endpoint_auto_confirms = true
+# }
 
 resource "aws_sns_topic_policy" "custom" {
   arn = "${aws_sns_topic.main.arn}"
@@ -30,12 +30,8 @@ resource "aws_sns_topic_policy" "custom" {
     "Effect": "Allow",
     "Principal": {"AWS":"*"},
     "Action": [
-      "sns:Publish",
-      "sns:GetTopicAttributes",
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:DescribeLogStreams"
+      "SNS:GetTopicAttributes",
+      "SNS:Publish"
     ],
     "Resource": "${aws_sns_topic.main.arn}"
   }]
