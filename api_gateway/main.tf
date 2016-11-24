995cb705c17d9b59ba332204dde40144aba8dea4
Antons Kranga <akranga@gmail.com>
2016-11-25 01:19:59 +0200

    cleaned up

--- api_gateway/main.tf
+++ api_gateway/main.tf
@@ -3,28 +3,6 @@ resource "aws_api_gateway_rest_api" "main" {
   description = "${var.description}"
 }
 
-#resource "aws_api_gateway_resource" "redshift_load" {
-#  rest_api_id = "${aws_api_gateway_rest_api.main.id}"
-#  parent_id = "${aws_api_gateway_rest_api.main.root_resource_id}"
-#  path_part = "load"
-#}
-
-#resource "aws_api_gateway_method" "redshift_load" {
-#  rest_api_id = "${aws_api_gateway_rest_api.main.id}"
-#  resource_id = "${aws_api_gateway_resource.redshift_load.id}"
-#  http_method = "GET"
-#  authorization = "NONE"
-#}
-
-#resource "aws_api_gateway_integration" "redshift_load" {
-#  rest_api_id = "${aws_api_gateway_rest_api.main.id}"
-#  resource_id = "${aws_api_gateway_resource.redshift_load.id}"
-#  http_method = "${aws_api_gateway_method.redshift_load.http_method}"
-#  type = "AWS"
-#  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${module.redshift_load.lambda_function_arn}/invocations"
-#  integration_http_method = "GET"
-#}
-
 resource "aws_api_gateway_method" "root" {
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   resource_id = "${aws_api_gateway_rest_api.main.root_resource_id}"
@@ -44,7 +22,7 @@ resource "aws_api_gateway_integration" "root" {
 
 resource "aws_api_gateway_api_key" "main" {
   name = "${var.name}-key"
-  description = "Cloud-analytics API gateway key"
+  description = "API gateway key"
 
   stage_key {
     rest_api_id = "${aws_api_gateway_rest_api.main.id}"
