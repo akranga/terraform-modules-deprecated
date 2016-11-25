04db351a9f34ea19dcd5fa5e1e05eee42563f79a
Antons Kranga <akranga@gmail.com>
2016-11-26 00:29:44 +0200

    added root_resource_output for apigateway

--- api_gateway/ouptut.tf
+++ api_gateway/ouptut.tf
@@ -2,6 +2,10 @@ output "api_id" {
 	value = "${aws_api_gateway_rest_api.main.id}"
 }
 
+output "root_resource_id" {
+  value = "${aws_api_gateway_rest_api.main.root_resource_id}"
+}
+
 output "stage_name" {
 	value = "${aws_api_gateway_deployment.dev.stage_name}"
 }
