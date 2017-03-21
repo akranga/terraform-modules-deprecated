d8ae368c5c56f896e70ab905032e93ccb9c3d36e
Antons Kranga <anton@agilestacks.com>
2017-03-21 13:45:01 +0200

    added service role outputs

--- aws_batch_compute_env/outputs.tf
+++ aws_batch_compute_env/outputs.tf
@@ -1,3 +1,11 @@
 output "name" {
   value = "${var.name}"
 }
+
+output "service_role_name" {
+  value = "${aws_iam_role.service_role.name}"
+}
+
+output "service_role_arn" {
+  value = "${aws_iam_role.service_role.arn}"
+}
