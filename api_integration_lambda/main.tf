8a96291688a6e09e614d8d10cec7c7c20f31ee03
Antons Kranga <akranga@gmail.com>
2017-03-23 11:17:42 +0200

    added error 400 and 500 error handlers

--- api_integration_lambda/main.tf
+++ api_integration_lambda/main.tf
@@ -31,7 +31,9 @@ resource "aws_api_gateway_integration_response" "200" {
   http_method       = "${aws_api_gateway_method.main.http_method}"
   status_code       = "${aws_api_gateway_method_response.200.status_code}"
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
   }
 }
 
@@ -40,8 +42,70 @@ resource "aws_api_gateway_method_response" "200" {
   resource_id = "${aws_api_gateway_resource.main.id}"
   http_method = "${aws_api_gateway_method.main.http_method}"
   status_code = "200"
+  response_parameters = { 
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
-# {aws_api_gateway_resource.main.path}
+
+resource "aws_api_gateway_integration_response" "400" {
+  rest_api_id = "${var.rest_api_id}"
+  resource_id = "${aws_api_gateway_resource.main.id}"
+  http_method = "${aws_api_gateway_method.main.http_method}"
+  status_code = "${aws_api_gateway_method_response.400.status_code}"
+  response_templates = {
+    "application/json" = <<EOF
+#set ($errorMessageObj = $util.parseJson($input.path('$.errorMessage')) {
+  "message" : "$errorMessageObj.message"
+}
+EOF
+  }
+  response_parameters = { 
+    "method.response.header.Access-Control-Allow-Origin" = "'${var.allow_origin}'" 
+  }
+}
+
+resource "aws_api_gateway_method_response" "400" {
+  rest_api_id = "${var.rest_api_id}"
+  resource_id = "${aws_api_gateway_resource.main.id}"
+  http_method = "${aws_api_gateway_method.main.http_method}"
+  status_code = "400"
+  response_models = { "application/json" = "Error" }
+  response_parameters = { 
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "500" {
+  rest_api_id = "${var.rest_api_id}"
+  resource_id = "${aws_api_gateway_resource.main.id}"
+  http_method = "${aws_api_gateway_method.main.http_method}"
+  status_code = "${aws_api_gateway_method_response.500.status_code}"
+  response_templates = {
+    "application/json" = <<EOF
+#set ($errorMessageObj = $util.parseJson($input.path('$.errorMessage')))
+{
+  "type" : "$errorMessageObj.errorType",
+  "message" : "$errorMessageObj.message",
+  "request-id" : "$errorMessageObj.requestId"
+}
+EOF
+  }
+  response_parameters = { 
+    "method.response.header.Access-Control-Allow-Origin" = "'${var.allow_origin}'" 
+  }
+}
+
+resource "aws_api_gateway_method_response" "500" {
+  rest_api_id = "${var.rest_api_id}"
+  resource_id = "${aws_api_gateway_resource.main.id}"
+  http_method = "${aws_api_gateway_method.main.http_method}"
+  status_code = "500"
+  response_models = { "application/json" = "Error" }
+  response_parameters = { 
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
 resource "aws_lambda_permission" "with_apig" {
   statement_id  = "${uuid()}"
   action        = "lambda:InvokeFunction"
