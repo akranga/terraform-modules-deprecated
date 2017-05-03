966288de754419dfa2f9481eb2c58130bec7dc6f
Antons Kranga <akranga@gmail.com>
2017-05-03 18:03:47 +0300

    unset current-context if it has been deleted

--- kube_config/unconfigure.sh
+++ kube_config/unconfigure.sh
@@ -2,3 +2,4 @@
 kubectl config delete-context ${cluster}
 kubectl config delete-cluster ${cluster}
 kubectl config unset users.admin@${cluster}
+[ $(kubectl config current-context) == "${cluster}" ] && kubectl config unset current-context
