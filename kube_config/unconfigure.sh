e7903352095f5c96835201fe4e97b063b2e83bcf
Antons Kranga <akranga@gmail.com>
2017-05-03 18:21:56 +0300

    improved setting kubeconfig context for deletion

--- kube_config/unconfigure.sh
+++ kube_config/unconfigure.sh
@@ -2,4 +2,15 @@
 kubectl config delete-context ${cluster}
 kubectl config delete-cluster ${cluster}
 kubectl config unset users.admin@${cluster}
-[ $(kubectl config current-context) == "${cluster}" ] && kubectl config unset current-context
+
+if [[ $(kubectl config current-context) == "${cluster}" ]]; then
+  kubectl config unset current-context
+  ctx=$(kubectl config view -o json | jq -r .contexts[0].name)
+  if [[ -z "$ctx"  ]]; then
+    kubectl config set-cluster local --server=http://localhost:8080
+    kubectl config set-context local --cluster=local --namespace=default
+    kubectl config use-context local
+  else
+    kubectl config use-context $ctx
+  fi
+fi
